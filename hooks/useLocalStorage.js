215bb875ca36eb349cab76396368787907b13b6e
Amarachi Joanna Egbuna <egbunajoanna16@gmail.com>
2025-08-05 12:08:01 +0100

    localstorage changes

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,18 +1,28 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 
-export const useLocalStorage = (key, intialValue) => {
-    
-  const getInitialValue = () => {
-    const stored = localStorage.getItem(key);
-    return stored ? JSON.parse(stored) : intialValue;
-  };
+export const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
 
-  const [value, setValue] = useState(getInitialValue);
+    if (typeof window === "undefined") return initialValue;
+
+    try {
+      const stored = localStorage.getItem(key);
+      return stored ? JSON.parse(stored) : initialValue;
+    } catch (error) {
+      console.error("Failed to get localstorage", error);
+    }
+  });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (typeof window === "undefined") return;
+
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Failed to write to localStorage", error);
+    }
   }, [key, value]);
 
   return [value, setValue];
